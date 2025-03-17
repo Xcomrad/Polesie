b18d5ac3af90a44b632567232bda10ef3a9e79dc
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-03-17 13:03:54 +0300

    init project

---
+++ Polecie/Polecie/PolecieApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolecieApp.swift
+//  Polecie
+//
+//  Created by Владислав Бут-Гусаим on 17.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PolecieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
