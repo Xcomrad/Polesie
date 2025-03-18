07f37bc8c0079b293d28144be31fcef3b49e273e
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-03-18 12:11:02 +0300

    добавил таббар и заглушки для экранов

---
+++ Polesie/Polesie/Modules/Quizzes/QuizzesView.swift
@@ -0,0 +1,19 @@
+//
+//  QuizzesView.swift
+//  Polesie
+//
+//  Created by Владислав Бут-Гусаим on 18.03.25.
+//
+
+import SwiftUI
+
+struct QuizzesView: View {
+    var body: some View {
+        Constants.Colors.background
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+#Preview {
+    QuizzesView()
+}
---
+++ Polesie/Polesie/Shared/Components/Tabbar/Tabbar.swift
@@ -0,0 +1,52 @@
+//
+//  Tabbar.swift
+//  Polesie
+//
+//  Created by Владислав Бут-Гусаим on 18.03.25.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    @Binding var selectedTab: Int
+    
+    var body: some View {
+        
+        HStack {
+            TabBarButton(
+                title: "История",
+                icon: "books.vertical.fill",
+                selectedIcon: Image(systemName: "books.vertical.fill"),
+                isSelected: selectedTab == 1,
+                action: { selectedTab = 1 }
+            )
+            
+            TabBarButton(
+                title: "Традиции",
+                icon: "person.2.fill",
+                selectedIcon: Image(systemName: "person.2.fill"),
+                isSelected: selectedTab == 2,
+                action: { selectedTab = 2 }
+            )
+            
+            TabBarButton(
+                title: "Квизы",
+                icon:"questionmark.square.fill",
+                selectedIcon: Image(systemName: "questionmark.square.fill"),
+                isSelected: selectedTab == 3,
+                action: { selectedTab = 3 }
+            )
+            
+            TabBarButton(
+                title: "Настройки",
+                icon: "gearshape.fill",
+                selectedIcon: Image(systemName: "gearshape.fill"),
+                isSelected: selectedTab == 4,
+                action: { selectedTab = 4 }
+            )
+        }
+        .padding()
+        .background(Constants.Colors.background)
+        .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: -5)
+    }
+}
