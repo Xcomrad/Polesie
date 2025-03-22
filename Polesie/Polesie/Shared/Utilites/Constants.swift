750e20aaa4a52effa3ceec1c4707b3e4aa7ea11d
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-03-22 15:05:21 +0300

    измененный английсикй + константы

--- Polesie/Polesie/Shared/Utilites/Constants.swift
+++ Polesie/Polesie/Shared/Utilites/Constants.swift
@@ -37,32 +37,37 @@ enum Constants {
     
     enum Fonts {
         // Заголовки
-        static let h1 = Font.custom("OldStandardTT-Regular", size: 32)
-        static let h1Bold = Font.custom("OldStandardTT-Bold", size: 32)
+        static let h1 = "OldStandardTT-Regular"
+        static let h1Bold = "OldStandardTT-Bold"
         
-        static let h2 = Font.custom("OldStandardTT-Regular", size: 28)
-        static let h2Bold = Font.custom("OldStandardTT-Bold", size: 28)
+        static let h2 = "OldStandardTT-Regular"
+        static let h2Bold = "OldStandardTT-Bold"
         
-        static let h3 = Font.custom("OldStandardTT-Regular", size: 24)
-        static let h3Bold = Font.custom("OldStandardTT-Bold", size: 24)
+        static let h3 = "OldStandardTT-Regular"
+        static let h3Bold = "OldStandardTT-Bold"
         
         // Основной текст
-        static let body = Font.custom("Lora-Regular", size: 20)
-        static let bodyBold = Font.custom("Lora-Bold", size: 20)
-        static let bodyItalic = Font.custom("Lora-Italic", size: 20)
+        static let body = "Lora-Regular"
+        static let bodyBold = "Lora-Bold"
+        static let bodyItalic = "Lora-Italic"
         
-        static let secondary = Font.custom("Lora-Regular", size: 18)
-        static let secondaryBold = Font.custom("Lora-Bold", size: 18)
+        static let secondary = "Lora-Regular"
+        static let secondaryBold = "Lora-Bold"
         
-        static let caption = Font.custom("Lora-Regular", size: 16)
-        static let captionBold = Font.custom("Lora-Bold", size: 16)
+        static let caption = "Lora-Regular"
+        static let captionBold = "Lora-Bold"
         
         // Интерфейс
-        static let button = Font.custom("Lora-Bold", size: 18)
-        static let small = Font.custom("Lora-Regular", size: 14)
+        static let button = "Lora-Bold"
+        static let small = "Lora-Regular"
+
+        static func scaledFont(name: String, baseSize: CGFloat, scale: CGFloat) -> Font {
+            let scaledSize = baseSize * scale
+            return .custom(name, size: scaledSize)
+        }
     }
     
-    enum Sizes {
+    enum PaddingSizes {
         static let cornerRadius: CGFloat = 10
         static let smalPadding: CGFloat = 16
         static let middlePadding: CGFloat = 24
