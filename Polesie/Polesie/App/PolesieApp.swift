3320d9fd1dfd15308504c209786262e1eba4673f
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-03-22 10:33:21 +0300

    выпилил смену локализации "on fly" - нарушает пользовательский опыт (смену локализации из настроек оставил)

--- Polesie/Polesie/App/PolesieApp.swift
+++ Polesie/Polesie/App/PolesieApp.swift
@@ -10,36 +10,14 @@ import SwiftUI
 @main
 struct PolesieApp: App {
     @StateObject private var darkModeManager = DarkModeManager()
-    @StateObject private var languageManager = LanguageManager.shared
     @AppStorage("isOnboardingCompleted") var isOnboardingCompleted: Bool = false
     
     var body: some Scene {
         WindowGroup {
-//            if isOnboardingCompleted {
-//                MainView()
-//                    .environmentObject(darkModeManager)
-//                    .preferredColorScheme(darkModeManager.isDarkMode ? .dark : .light)
-//            } else {
-//                OnboardingView(steps: steps)
-//            }
+
             SplashView()
                 .environmentObject(darkModeManager)
-                .environmentObject(LanguageManager.shared)
                 .preferredColorScheme(darkModeManager.isDarkMode ? .dark : .light)
         }
     }
 }
-
-//let steps: [OnboardingModel] = [OnboardingModel(image: "books.vertical.fill",
-//                                           title: "История",
-//                                           description: "Нажав на нее вы сможете перейти во вкладку истории Полесья и всякой неведомой херни."),
-//                           OnboardingModel(image: "scroll.fill",
-//                                           title: "Традиции",
-//                                           description: "Здесь предоставлена информация о трациях, праздниках и культуре Полесья."),
-//                           OnboardingModel(image: "questionmark.bubble.fill",
-//                                           title: "Квизы!",
-//                                           description: "A тут вы сможете проверить свои знания! \nНе беспокойтесь, мы никому не скажем, если ошибетёсь..."),
-//                           OnboardingModel(image: "gearshape.fill",
-//                                           title: "Настройки",
-//                                           description: "Помогут вам настроить приложение по вашему усмотрению."),
-//]
--- Polesie/Polesie/Modules/Settings/View/SettingsView.swift
+++ Polesie/Polesie/Modules/Settings/View/SettingsView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct SettingsView: View {
     @EnvironmentObject var darkModeManager: DarkModeManager
-    @EnvironmentObject var languageManager: LanguageManager
     @StateObject private var vm: SettingsViewModel
     
     init(vm: SettingsViewModel) {
@@ -23,26 +22,13 @@ struct SettingsView: View {
                     Toggle(Constants.Strings.settingsThemeToggle, isOn: $darkModeManager.isDarkMode)
                         .font(Constants.Fonts.button)
                 }
-            
-            Section(header: Text(Constants.Strings.settingsLanguageTitle)
-                .font(Constants.Fonts.secondaryBold)) {
-                    Picker(languageManager.localizedString(forKey: Constants.Strings.settingsLanguage),
-                           selection: $languageManager.currentLanguage) {
-                        ForEach(languageManager.availableLanguages, id: \.self) { languageCode in
-                            Text(self.vm.languageName(for: languageCode))
-                                .tag(languageCode)
-                        }
-                    }
-                           .pickerStyle(.menu)
-                }
         }
     }
 }
 
 #Preview {
     SettingsView(vm: SettingsViewModel())
-        .environmentObject(DarkModeManager.shared)
-        .environmentObject(LanguageManager.shared)
+        .environmentObject(DarkModeManager())
 }
 
 
