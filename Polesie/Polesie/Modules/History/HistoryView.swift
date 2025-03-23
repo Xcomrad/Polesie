af96b754c54fb54a21cb35e0e2b3760db630db90
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-03-23 16:00:02 +0300

    мок для сайдбара + заглушка экрана истрии

--- Polesie/Polesie/Modules/History/HistoryView.swift
+++ Polesie/Polesie/Modules/History/HistoryView.swift
@@ -9,16 +9,49 @@ import SwiftUI
 
 struct HistoryView: View {
     @EnvironmentObject var fontSizeManager: FontSizeManager
+    @Environment(\.colorScheme) var colorScheme
+    @State private var isSidebarVisible = false
     
     var body: some View {
         ZStack {
             Constants.Colors.background.opacity(0.5).ignoresSafeArea(.all)
             VStack {
-                Text("HistoryView")
-                    .font(Constants.DynamicFonts.scaledFont(name: Constants.DynamicFonts.h1, baseSize: 32, scale: fontSizeManager.fontSizeScale))
-                Text("HistoryView")
-                    .font(.system(size: 10))
+                // Кнопка для открытия меню
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        isSidebarVisible.toggle()
+                    }
+                }) {
+                    Image(systemName: "line.horizontal.3")
+                        .font(.title)
+                        .foregroundStyle(Constants.Colors.accent)
+                        .padding()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                // Основной экран
+                Text("Экран истории")
+                    .font(.largeTitle)
+                    .padding()
+                
                 Spacer()
+            }
+            
+            if isSidebarVisible {
+                Constants.Colors.background.ignoresSafeArea(.all)
+                    .onTapGesture {
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            isSidebarVisible.toggle()
+                        }
+                    }
+            }
+            
+            // Боковое меню
+            if isSidebarVisible {
+                SidebarView(isVisible: $isSidebarVisible)
+                    .transition(.move(edge: .leading))
+                    .zIndex(1)
+                    .offset(x: isSidebarVisible ? 0 : UIScreen.main.bounds.width / 2)
             }
         }
     }
