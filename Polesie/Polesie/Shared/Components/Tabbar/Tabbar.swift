cc5bee112bc82e38f3baf23cba34c42f3a0c2953
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-03-23 11:42:16 +0300

    рефакторинг Tabbar

--- Polesie/Polesie/Shared/Components/Tabbar/Tabbar.swift
+++ Polesie/Polesie/Shared/Components/Tabbar/Tabbar.swift
@@ -12,47 +12,50 @@ struct TabBar: View {
     @Binding var selectedTab: Int
     
     var body: some View {
-        
-        HStack {
-            TabBarButton(
-                title: Constants.Strings.historyButtonTitle,
-                icon: "books.vertical",
-                selectedIcon: "books.vertical.fill",
-                isSelected: selectedTab == 0,
-                action: { selectedTab = 0 }
+        VStack {
+            Spacer()
+            HStack {
+                TabBarButton(
+                    title: Constants.Strings.historyButtonTitle,
+                    icon: "books.vertical",
+                    selectedIcon: "books.vertical.fill",
+                    isSelected: selectedTab == 0,
+                    action: { selectedTab = 0 }
+                )
+                
+                TabBarButton(
+                    title: Constants.Strings.traditionsButtonTitle,
+                    icon: "scroll",
+                    selectedIcon: "scroll.fill",
+                    isSelected: selectedTab == 1,
+                    action: { selectedTab = 1 }
+                )
+                
+                TabBarButton(
+                    title: Constants.Strings.quizzesButtonTitle,
+                    icon: "questionmark.bubble",
+                    selectedIcon: "questionmark.bubble.fill",
+                    isSelected: selectedTab == 2,
+                    action: { selectedTab = 2 }
+                )
+                
+                TabBarButton(
+                    title: Constants.Strings.settingsButtonTitle,
+                    icon: "gearshape",
+                    selectedIcon: "gearshape.fill",
+                    isSelected: selectedTab == 3,
+                    action: { selectedTab = 3 }
+                )
+            }
+            .padding(.vertical, Constants.PaddingSizes.cornerRadius)
+            .padding(.horizontal, Constants.PaddingSizes.middlePadding)
+            .background(
+                RoundedRectangle(cornerRadius: Constants.PaddingSizes.cornerRadius)
+                    .fill(Constants.Colors.background)
+                    .adaptiveShadow(colorScheme: colorScheme)
             )
-            
-            TabBarButton(
-                title: Constants.Strings.traditionsButtonTitle,
-                icon: "scroll",
-                selectedIcon: "scroll.fill",
-                isSelected: selectedTab == 1,
-                action: { selectedTab = 1 }
-            )
-            
-            TabBarButton(
-                title: Constants.Strings.quizzesButtonTitle,
-                icon: "questionmark.bubble",
-                selectedIcon: "questionmark.bubble.fill",
-                isSelected: selectedTab == 2,
-                action: { selectedTab = 2 }
-            )
-            
-            TabBarButton(
-                title: Constants.Strings.settingsButtonTitle,
-                icon: "gearshape",
-                selectedIcon: "gearshape.fill",
-                isSelected: selectedTab == 3,
-                action: { selectedTab = 3 }
-            )
+            .padding(.horizontal, Constants.PaddingSizes.cornerRadius)
+            .ignoresSafeArea(.all, edges: .bottom) 
         }
-        .padding(.vertical, Constants.PaddingSizes.cornerRadius)
-        .padding(.horizontal, Constants.PaddingSizes.middlePadding)
-        .background(
-            RoundedRectangle(cornerRadius: Constants.PaddingSizes.cornerRadius)
-                .fill(Constants.Colors.background)
-                .adaptiveShadow(colorScheme: colorScheme)
-        )
-        .padding(.horizontal, Constants.PaddingSizes.cornerRadius)
     }
 }
