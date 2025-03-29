65acdb2d10cb6224151a40da8c3b36221667cd63
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-03-29 11:22:43 +0300

    рефакторинг

--- Polesie/Polesie/Shared/Components/SideBar/SidebarView.swift
+++ Polesie/Polesie/Shared/Components/SideBar/SidebarView.swift
@@ -8,52 +8,64 @@
 import SwiftUI
 
 struct SidebarView: View {
+    private let sidebarWidh = UIScreen.main.bounds.width * 0.4
     @Environment(\.colorScheme) var colorScheme
     @Binding var isVisible: Bool
     
     var body: some View {
         ZStack(alignment: .leading) {
             VStack(alignment: .leading, spacing: Constants.PaddingSizes.middlePadding) {
-                Button(action: {
-                    withAnimation(.easeInOut(duration: 0.5)) {
-                        isVisible.toggle()
-                    }
-                }) {
-                    Image(systemName: "xmark")
-                        .font(.title)
-                        .foregroundStyle(Constants.Colors.accent)
-                        .padding()
-                }
-                
-                Text("Темы")
-                    .font(Constants.BaseFonts.h2Bold)
-                    .foregroundStyle(Constants.Colors.text)
-                    .padding(.horizontal)
-                
-                // Список тем
-                ScrollView {
-                    VStack(spacing: Constants.PaddingSizes.cornerRadius) {
-                        ForEach(["История", "События", "Люди", "Ремесло", "Места", "Природа"], id: \.self) { topic in
-                            SidebarButton(title: topic, action: {
-                                print("Выбрана тема: \(topic)")
-                                withAnimation(.easeInOut(duration: 0.5)) {
-                                    isVisible.toggle()
-                                }
-                            })
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                    }
-                    .padding(Constants.PaddingSizes.smalPadding)
-                }
-                .scrollDisabled(true)
-                
+                clouseButton
+                themeTitle
+                scrollView
                 Spacer()
             }
-            .frame(width: UIScreen.main.bounds.width * 0.4) // Ширина меню 40%
+            .frame(width: sidebarWidh)
             .background(Constants.Colors.background.opacity(0.5))
         }
         .adaptiveShadow(colorScheme: colorScheme)
-        .frame(width: UIScreen.main.bounds.width * 0.4) // Ширина меню 40%
-        .offset(x: isVisible ? -UIScreen.main.bounds.width * 0.3 : -UIScreen.main.bounds.width) 
+        .frame(width: sidebarWidh)
+        .offset(x: isVisible ? -UIScreen.main.bounds.width * 0.3 : -UIScreen.main.bounds.width)
+    }
+    
+    //MARK: - Components
+    
+    private var clouseButton: some View {
+        Button(action: toggleSidebar) {
+            Image(systemName: "xmark")
+                .font(.title)
+                .foregroundStyle(Constants.Colors.accent)
+                .padding()
+        }
+    }
+    
+    private var themeTitle: some View {
+        Text("Темы")
+            .font(Constants.BaseFonts.h2Bold)
+            .foregroundStyle(Constants.Colors.text)
+            .padding(.horizontal)
+    }
+    
+    private var scrollView: some View {
+        ScrollView {
+            LazyVStack(spacing: Constants.PaddingSizes.cornerRadius) {
+                ForEach(["История", "События", "Люди", "Ремесло", "Места", "Природа"], id: \.self) { topic in
+                    SidebarButton(title: topic, action: {
+                        print("Выбрана тема: \(topic)")
+                        toggleSidebar()
+                    })
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+            .padding(Constants.PaddingSizes.smalPadding)
+        }
+        .scrollDisabled(true)
+    }
+    
+    //MARK: - Actions
+    private func toggleSidebar() {
+        withAnimation(.easeInOut(duration: 0.5)) {
+            isVisible.toggle()
+        }
     }
 }
