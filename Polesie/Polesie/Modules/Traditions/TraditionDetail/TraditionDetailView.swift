16e7938a7e006f7732e3be08e14afd3cd2845087
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-04-09 14:26:04 +0300

    детальный экран традиций

---
+++ Polesie/Polesie/Modules/Traditions/TraditionDetail/TraditionDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  TraditionDetailView.swift
+//  Polesie
+//
+//  Created by Владислав Бут-Гусаим on 9.04.25.
+//
+
+import SwiftUI
+
+struct TraditionDetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
