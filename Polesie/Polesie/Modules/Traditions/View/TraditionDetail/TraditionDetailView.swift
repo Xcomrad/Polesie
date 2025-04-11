d77f21f8f83707a2c6d33a0e3e93b0266d3df22b
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-04-11 19:55:03 +0300

    добавил детальные экраны традиций + json

--- Polesie/Polesie/Modules/Traditions/View/TraditionDetail/TraditionDetailView.swift
+++ Polesie/Polesie/Modules/Traditions/View/TraditionDetail/TraditionDetailView.swift
@@ -1,31 +1,90 @@
 //
-//  TraditionDetailView.swift
+//  TraditionItemView.swift
 //  Polesie
 //
-//  Created by Владислав Бут-Гусаим on 9.04.25.
+//  Created by Владислав Бут-Гусаим on 11.04.25.
 //
 
 import SwiftUI
 
 struct TraditionDetailView: View {
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject var vm: TraditionsViewModel
+    
+    @State private var isPressed = false
+    @State private var scrollToTop: Bool = false
+    
+    var title: String
+    var description: String
+    
     var body: some View {
-        ScrollView {
-            LazyVStack(alignment: .leading, spacing: 0, pinnedViews: .sectionHeaders) {
-                headerView
+        ScrollViewReader { proxy in
+            ZStack {
+                Constants.Colors.background
+                    .opacity(Constants.PaddingSizes.p05)
+                    .ignoresSafeArea(.all)
+            ScrollView {
+               
+                    VStack(spacing: Constants.PaddingSizes.p16) {
+                        
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "xmark.app.fill")
+                                .resizable()
+                                .scaledToFill()
+                                .foregroundStyle(Constants.Colors.accent)
+                                .frame(width: Constants.PaddingSizes.p24,
+                                       height: Constants.PaddingSizes.p24)
+                        }
+                        .adaptiveShadow(colorScheme: colorScheme)
+                        .padding(.top, Constants.PaddingSizes.p8)
+                        .padding(.leading, Constants.PaddingSizes.p8)
+                        
+                        traditionTitle
+                        traditionDescription
+                       
+                        Button(action: {
+                            withAnimation {
+                                proxy.scrollTo("top", anchor: .top)
+                            }
+                        }) {
+                            Image(systemName: "arrowshape.up.fill")
+                                .resizable()
+                                .scaledToFill()
+                                .foregroundStyle(Constants.Colors.accent)
+                                .frame(width: Constants.PaddingSizes.p24,
+                                       height: Constants.PaddingSizes.p24)
+                        }
+                        .adaptiveShadow(colorScheme: colorScheme)
+                        .padding(.bottom, Constants.PaddingSizes.p8)
+                        .padding(.trailing, Constants.PaddingSizes.p8)
+                    }
+                    .padding()
+                    .id("top")
+                }
             }
+        }
+        .onAppear {
+            vm.fetchData()
         }
     }
     
     // MARK: - Components
-    private var headerView: some View {
-        Image("")
+    private var traditionTitle: some View {
+        Text(title)
+            .font(Constants.BaseFonts.h1Bold)
+            .foregroundColor(Constants.Colors.text)
+            .padding(.horizontal)
     }
     
-    private var description: some View {
-        Text("")
+    private var traditionDescription: some View {
+        Text(description)
+            .font(Constants.BaseFonts.body)
+            .foregroundColor(Constants.Colors.text)
+            .lineSpacing(6)
+            .padding(.horizontal)
     }
 }
 
-#Preview {
-    TraditionDetailView()
-}
