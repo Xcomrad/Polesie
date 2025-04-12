6ec44a07ce44030b1420a0b8517558226d9a5fb5
Бут-Гусаим Владислав Валерьевич (ООО Бэлл Интегратор Инновации) <@out_But-GusaimVlV>
2025-04-12 11:04:54 +0300

    поправил навигацию, решил баг с darkmode, расширил json

--- Polesie/Polesie/Modules/Traditions/View/TraditionDetail/TraditionDetailView.swift
+++ Polesie/Polesie/Modules/Traditions/View/TraditionDetail/TraditionDetailView.swift
@@ -18,12 +18,11 @@ struct TraditionDetailView: View {
     var description: String
     
     var body: some View {
-        ScrollViewReader { proxy in
+        NavigationStack {
             ZStack {
                 Constants.Colors.background
                     .opacity(Constants.PaddingSizes.p05)
                     .ignoresSafeArea(.all)
-        
                 ScrollView {
                     VStack(spacing: Constants.PaddingSizes.p16) {
                         traditionTitle
@@ -35,79 +34,53 @@ struct TraditionDetailView: View {
                 }
                 
                 closeButton
-                scrollToTopButton(proxy: proxy)
             }
         }
+        .navigationBarBackButtonHidden(true)
         .onAppear {
             vm.fetchData()
         }
-    }
-    
-    // MARK: - Components
-    private var traditionTitle: some View {
-        Text(title)
-            .font(Constants.BaseFonts.h1Bold)
-            .foregroundColor(Constants.Colors.text)
-            .multilineTextAlignment(.center)
-            .padding(.horizontal)
-    }
-    
-    private var traditionDescription: some View {
-        Text(description)
-            .font(Constants.BaseFonts.body)
-            .foregroundColor(Constants.Colors.text)
-            .lineSpacing(6)
-            .padding(.horizontal)
-    }
-    
-    private var closeButton: some View {
-        VStack {
-            HStack {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "xmark.app.fill")
-                        .resizable()
-                        .scaledToFill()
-                        .foregroundStyle(Constants.Colors.accent)
-                        .frame(
-                            width: Constants.PaddingSizes.p24,
-                            height: Constants.PaddingSizes.p24
-                        )
-                }
-                .adaptiveShadow(colorScheme: colorScheme)
-                Spacer()
+}
+
+// MARK: - Components
+private var traditionTitle: some View {
+    Text(title)
+        .font(Constants.BaseFonts.h1Bold)
+        .foregroundColor(Constants.Colors.text)
+        .multilineTextAlignment(.center)
+        .padding(.horizontal)
+}
+
+private var traditionDescription: some View {
+    Text(description)
+        .font(Constants.BaseFonts.body)
+        .foregroundColor(Constants.Colors.text)
+        .lineSpacing(6)
+        .padding(.horizontal)
+}
+
+private var closeButton: some View {
+    VStack {
+        HStack {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "arrowshape.backward.fill")
+                    .resizable()
+                    .scaledToFill()
+                    .foregroundStyle(Constants.Colors.accent)
+                    .frame(
+                        width: Constants.PaddingSizes.p24,
+                        height: Constants.PaddingSizes.p24
+                    )
             }
-            .padding(.top, Constants.PaddingSizes.p12)
-            .padding(.leading, Constants.PaddingSizes.p24)
-            
+            .adaptiveShadow(colorScheme: colorScheme)
             Spacer()
         }
+        .padding(.top, Constants.PaddingSizes.p12)
+        .padding(.leading, Constants.PaddingSizes.p24)
+        
+        Spacer()
     }
-    
-    private func scrollToTopButton(proxy: ScrollViewProxy) -> some View {
-        VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                Button {
-                    withAnimation {
-                        proxy.scrollTo("top", anchor: .top)
-                    }
-                } label: {
-                    Image(systemName: "arrowshape.up.fill")
-                        .resizable()
-                        .scaledToFill()
-                        .foregroundStyle(Constants.Colors.accent)
-                        .frame(
-                            width: Constants.PaddingSizes.p24,
-                            height: Constants.PaddingSizes.p24
-                        )
-                }
-                .adaptiveShadow(colorScheme: colorScheme)
-                .padding(.bottom, Constants.PaddingSizes.p12)
-                .padding(.trailing, Constants.PaddingSizes.p24)
-            }
-        }
-    }
+}
 }
